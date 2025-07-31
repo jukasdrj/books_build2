f80b9e789c8e1eae4f2db139d9d6d2d2ae37d7fa
jukasdrj <jukasdrj@gmail.com>
2025-07-31 17:10:50 -0500

    Updated theme color references to use Theme/ namespace and renamed SubtleBackground to Surface

--- books/Theme.swift
+++ books/Theme.swift
@@ -1,9 +1,9 @@
 import SwiftUI
 
 extension Color {
-    static let primaryText = Color("PrimaryText")
-    static let secondaryText = Color("SecondaryText")
-    static let primaryAction = Color("PrimaryAction")
-    static let accentHighlight = Color("AccentHighlight")
-    static let subtleBackground = Color("SubtleBackground")
+    static let primaryText = Color("Theme/PrimaryText")
+    static let secondaryText = Color("Theme/SecondaryText")
+    static let primaryAction = Color("Theme/PrimaryAction")
+    static let accentHighlight = Color("Theme/AccentHighlight")
+    static let subtleBackground = Color("Theme/Surface")
 }
